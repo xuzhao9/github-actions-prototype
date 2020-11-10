6e56ce5a7cf044058dfab8cd9789b2245199b89f
Xu Zhao <xzhao9@fb.com>
2020-11-09 20:52:28 -0500

    Added new scripts.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-now=`date +%s`
-echo "Hello from GH Actions!" > ${HOME}/result/${now}.txt
+set -euo pipefail
+
+# Root of this repo
+REPO_ROOT=`pwd`/../
+# Directory that stores the json results
+RESULTS_DIR=${HOME}/benchmark-results
+# Directory that stores the nightly figures
+FIGURES_DIR=${HOME}/artifact-figures
+
+# Run the Docker
+
+# We allow each nightly build to be run multiple times
+# We use the latest run for each nightly build to generate the figure
+
+# Link the artifacts 
+# rm ${FIGURES_DIR}/artifact-figure-latest.html
+# ln -s ${FIGURES_DIR}/${LATEST_FIGURE} ${FIGURES_DIR}/artifact-figure-latest.html
 
