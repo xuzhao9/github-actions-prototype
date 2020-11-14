3325be111f858169c56419bef372b02968f73846
Xu Zhao <xzhao9@fb.com>
2020-11-13 19:13:47 -0500

    Fix bug in build benchmark docker.

--- scripts/build-benchmark-docker.sh
+++ scripts/build-benchmark-docker.sh
@@ -21,7 +21,9 @@ TODAY=$(date +"%Y%m%d")
 TORCH_LATEST=$(docker images | grep "pytorch\s" | head -n 1)
 TORCH_ID=$(echo $TORCH_LATEST | tr -s ' ' | cut -d ' ' -f3)
 
+echo "Building benchmark docker using pytorch docker $TORCH_ID..."
+
 pushd ${BENCHMARK_SRC}
-sed -i 's,ec2-user/pytorch,$TORCH_ID' docker/Dockerfile
+sed -i 's,ec2-user/pytorch,$TORCH_ID,' docker/Dockerfile
 docker build -t $(id -un)/pytorch-benchmark:$TODAY docker
-sed -i 's,$TORCH_ID,ec2-user/pytorch' docker/Dockerfile
+sed -i 's,$TORCH_ID,ec2-user/pytorch,' docker/Dockerfile
