9b2880aa1be006511f58131f839c8db41dd7db31
Xu Zhao <xzhao9@fb.com>
2020-11-19 16:01:08 -0500

    Fix jq env.

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -2,16 +2,18 @@
 
 set -eo pipefail
 
+NUM_ITER=1
+NUM_ROUNDS=20
+DATA_JSON_PREFIX=$(date +"%Y%m%d_%H%M%S")
+BENCHMARK_FILTER="(BERT_pytorch)"
+# BENCHMARK_FILTER="(not slomo)"
+
 # This machine has 16 physical cores, we use 12 of them to test
 CORE_LIST="4-15"
 export GOMP_CPU_AFFINITY="${CORE_LIST}"
 export CUDA_VISIBLE_DEVICES=0
 
-NUM_ITER=1
 NO_TURBO_FILE="/sys/devices/system/cpu/intel_pstate/no_turbo"
-DATA_JSON_PREFIX=$(date +"%Y%m%d_%H%M%S")
-BENCHMARK_FILTER="(BERT_pytorch)"
-# BENCHMARK_FILTER="(not slomo)"
 
 conda init bash; conda run /bin/bash
 
@@ -23,10 +25,10 @@ pushd /workspace/benchmark
 
 for c in $(seq 1 $NUM_ITER); do
     echo "Run pytorch/benchmark for ${TORCH_VER} iter ${c}"
-    taskset -c "${CORE_LIST}" pytest test_bench.py -k "${BENCHMARK_FILTER}" --benchmark-min-rounds 20 \
+    taskset -c "${CORE_LIST}" pytest test_bench.py -k "${BENCHMARK_FILTER}" --benchmark-min-rounds "${NUM_ROUNDS}" \
                               --benchmark-json /output/${DATA_JSON_PREFIX}_${c}.json
     # Fill in circle_build_num and circle_project_reponame
-    jq '.machine_info.circle_project_name=githubactions-benchmark | .machine_info.circle_build_num=${GITHUB_RUN_ID}' \
+    jq --arg run_id "${GITHUB_RUN_ID}" '.machine_info.circle_project_name=githubactions-benchmark | .machine_info.circle_build_num=$run_id' \
        /output/${DATA_JSON_PREFIX}_${c}.json > /output/${DATA_JSON_PREFIX}_${c}.json.tmp
     mv /output/${DATA_JSON_PREFIX}_${c}.json.tmp /output/${DATA_JSON_PREFIX}_${c}.json
 done
