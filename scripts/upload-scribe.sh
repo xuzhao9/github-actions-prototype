a1a8c40087f047f9320f49be1b75b27258bfa9ee
Xu Zhao <xzhao9@fb.com>
2020-11-19 18:26:28 -0500

    Added upload-scribe.

--- scripts/upload-scribe.sh
+++ scripts/upload-scribe.sh
@@ -4,9 +4,14 @@
 
 set -eo pipefail
 
-pushd /workspace/benchmark
+pushd /workspace/benchmark/score
 
-TORCHBENCH_SCORE=$(python score/compute_score.py --benchmark_data_dir /output)
+TORCHBENCH_SCORE=$(python compute_score.py --benchmark_data_dir /output | awk 'NR>2' )
 
-find /output -name ".json" -type f -exec \
-     python3 ./scripts/upload_scribe.py --pytest_bench_json {} --torchbench_score $TORCHBENCH_SCORE \;
+IFS=$'\n'
+for line in $TORCHBENCH_SCORE ; do
+    JSON_NAME=$(echo $line | tr -s " " | cut -d " " -f 1)
+    SCORE=$(echo $line | tr -s " " | cut -d " " -f 2)
+    python3 ./scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
+            --torchbench_score $SCORE
+done
