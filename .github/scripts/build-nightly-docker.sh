2b9a53c5629434eef474aa7059ebe3f85569aa21
Xu Zhao <xzhao9@fb.com>
2021-02-04 18:29:27 -0500

    Fix another bug in nightly.

--- .github/scripts/build-nightly-docker.sh
+++ .github/scripts/build-nightly-docker.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-set -euo pipefail
+set -xeuo pipefail
 
 PYTORCH_GITHUB="https://github.com/pytorch/pytorch.git"
 PYTORCH_SRC=pytorch
 
-git clone -b master --single-branch $PYTORCH_GITHUB $PYTORCH_SRC
+git clone -b nightly --single-branch $PYTORCH_GITHUB $PYTORCH_SRC
 
 # Build the nightly docker
 pushd $PYTORCH_SRC
+# Docker full name is: ghcr.io/pytorch-nightly/pytorch:${PYTORCH_NIGHTLY_COMMIT}
+PYTORCH_NIGHTLY_COMMIT=$(git log -1 --pretty=%B | head -1 | sed 's,.*(\([[:xdigit:]]*\)),\1,' | head -c 7)
 make -f docker.Makefile \
+     DOCKER_REGISTRY=ghcr.io \
+     DOCKER_ORG=pytorch-nightly \
+     DOCKER_TAG=${PYTORCH_NIGHTLY_COMMIT} \
      INSTALL_CHANNEL=pytorch-nightly BUILD_TYPE=official devel-image
 popd
