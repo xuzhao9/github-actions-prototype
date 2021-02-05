8057bac55eb33ac00729b9f4590fc7caeff0f15a
Xu Zhao <xzhao9@fb.com>
2021-02-05 18:30:54 -0500

    Add another test.

--- .github/scripts/instr-count-benchmark/create-nightly-env.sh
+++ .github/scripts/instr-count-benchmark/create-nightly-env.sh
@@ -8,11 +8,11 @@ source ${BASEDIR}/config.env
 set +a;
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/miniconda3
 
+conda init bash; conda run /bin/bash
 conda create -y -q --name ${CONDA_ENV_NAME} python=${PYTHON_VERSION}
-conda init bash
 
 # Install PyTorch nightly from pip
-conda run -n ${CONDA_ENV_NAME} pip install --pre torch \
-    -f https://download.pytorch.org/whl/nightly/${CUDA_VERSION}/torch_nightly.html
+# conda run -n ${CONDA_ENV_NAME} pip install --pre torch \
+#     -f https://download.pytorch.org/whl/nightly/${CUDA_VERSION}/torch_nightly.html
