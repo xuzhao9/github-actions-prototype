2488182acb1c900aef5343a86fbbaa3728ee35e4
Xu Zhao <xzhao9@fb.com>
2021-02-05 18:17:39 -0500

    Another try to activate environment.

--- .github/scripts/instr-count-benchmark/create-nightly-env.sh
+++ .github/scripts/instr-count-benchmark/create-nightly-env.sh
@@ -13,8 +13,7 @@ bash Miniconda3-latest-Linux-x86_64.sh -b
 conda create -y -q --name ${CONDA_ENV_NAME} python=${PYTHON_VERSION}
 conda init bash; conda run /bin/bash
 conda run -n ${CONDA_ENV_NAME} /bin/bash
-conda activate ${CONDA_ENV_NAME}
 
 # Install PyTorch nightly from pip
-pip install --pre torch \
+conda run -n ${CONDA_ENV_NAME} pip install --pre torch \
     -f https://download.pytorch.org/whl/nightly/${CUDA_VERSION}/torch_nightly.html
