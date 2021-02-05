823beb7fc301cdedf68feb6d3a4edae87c7849ac
Xu Zhao <xzhao9@fb.com>
2021-02-05 17:14:18 -0500

    Activate the conda env.

--- .github/scripts/instr-count-benchmark/create-nightly-env.sh
+++ .github/scripts/instr-count-benchmark/create-nightly-env.sh
@@ -11,7 +11,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
 
 conda create -y -q --name ${CONDA_ENV_NAME} python=${PYTHON_VERSION}
-. activate ${CONDA_ENV_NAME}
+conda activate ${CONDA_ENV_NAME}
 conda init bash; conda run /bin/bash
 
 # Install PyTorch nightly from pip
