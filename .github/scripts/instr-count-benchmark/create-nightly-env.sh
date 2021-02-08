10f42a98f0184caa447f2c4c9c2ca83b671c19fe
Xu Zhao <xzhao9@fb.com>
2021-02-08 15:58:22 -0500

    Push changes to instr count.

--- .github/scripts/instr-count-benchmark/create-nightly-env.sh
+++ .github/scripts/instr-count-benchmark/create-nightly-env.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
+
+  #!/bin/sh
 
 set -xueo pipefail
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/miniconda
+# =========== END of prelude ===========
 
 BASEDIR=$(dirname $0)
 set -a;
 source ${BASEDIR}/config.env
 set +a;
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/miniconda
-
-# source ${HOME}/miniconda/etc/profile.d/conda.sh
-source ${HOME}/.bashrc
-
 conda create -y -q --name ${CONDA_ENV_NAME} python=${PYTHON_VERSION}
-# Install PyTorch nightly from pip
 conda activate ${CONDA_ENV_NAME}
+
+# Install PyTorch nightly from pip
 pip install --pre torch \
     -f https://download.pytorch.org/whl/nightly/${CUDA_VERSION}/torch_nightly.html
--- .github/scripts/instr-count-benchmark/run-benchmark.sh
+++ .github/scripts/instr-count-benchmark/run-benchmark.sh
@@ -7,7 +7,9 @@ set -a;
 source ${BASEDIR}/config.env
 set +a;
 
-# Run the instruction count benchmark
-eval "$(${CONDA_PREFIX}/bin/conda shell.bash hook)"
 conda activate ${CONDA_ENV_NAME}
-conda deactivate
+PYTORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
+PYTORCH_GIT_VERSION=$(python -c "import torch; print(torch.version.git_version)" | head -c 7)
+echo "Running instruction benchmark for pytorch-${PYTORCH_VERSION}, commit SHA: ${PYTORCH_GIT_VERSION}"
+
+# Run the instruction count benchmark
