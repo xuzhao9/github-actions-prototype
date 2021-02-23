1bcb2eea4a2bc16d059634294e813d8c94679c75
Xu Zhao <xzhao9@fb.com>
2021-02-23 13:45:43 -0500

    Added exit code.

--- .github/scripts/monitor_proc.sh
+++ .github/scripts/monitor_proc.sh
@@ -93,7 +93,7 @@ fi
 echo "Running  ${COMMAND_TO_RUN} > \"${LOG_FILE}\" 2>&1 &"
 /usr/bin/time -f "%e" ${COMMAND_TO_RUN} > "${LOG_FILE}" 2>&1 &
 PID_TO_WATCH=$(ps aux |  grep -v 'grep' | grep -F "${COMMAND_TO_RUN}" | grep -v "$0" | awk '{print $2}' | head -1)
-trap "kill -9 ${PID_TO_WATCH} >/dev/null 2>/dev/null" $(seq 0 15)
+trap "kill -9 ${PID_TO_WATCH} >/dev/null 2>/dev/null || exit 0" $(seq 0 15)
 MAX_GPU_MEMORY=0
 MAX_RSS_MEMORY=0
 MAX_PSS_MEMORY=0
