355b461a845ebfe1e5934eba026376920974a449
Xu Zhao <i@xuzhao.net>
2021-09-16 21:47:42 -0400

    Another main.py fix

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import subprocess
+import os
 
 print("This is an output.")
 
-# Set the environment variable
-command = 'echo TORCHBENCH_BISECT="YES" >> $GITHUB_ENV'
-subprocess.check_call(command, shell=False)
+e = os.environ["GITHUB_ENV"]
+print(e)
+e = e + 'TORCHBENCH_BISECT="YES"' + "\n"
+os.environ["GITHUB_ENV"] = e
+print(e)
 
